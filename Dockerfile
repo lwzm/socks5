00e0a3effdad4856cedc22f8999a4f01e1aebf4a
lwzm <lwzm@qq.com>
2019-12-09 11:38:52 +0800

    auto generate key-pair, and display public key

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,7 @@ FROM alpine
 LABEL maintainer="lwzm@qq.com"
 
 RUN apk add --no-cache openssh-client
+COPY entrypoint.sh /bin/
+
 EXPOSE 1080
-ENTRYPOINT [ "ssh", "-o", "StrictHostKeyChecking=no", "-o", "PreferredAuthentications=publickey", "-i", "id_rsa", "-N", "-D", ":1080" ]
+ENTRYPOINT [ "entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+ID_RSA=${ID_RSA:-/id_rsa}
+OPTS=" -o StrictHostKeyChecking=no -o PreferredAuthentications=publickey -o ServerAliveInterval=60 -o ServerAliveCountMax=2"
+
+
+if [ ! -f "$ID_RSA" ]; then
+    ssh-keygen -q -P '' -f "$ID_RSA"
+fi
+
+while true; do
+    ssh -T $OPTS -i "$ID_RSA" "$@" </dev/null
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    date
+    echo "=== todo: public key ==="
+    ssh-keygen -y -f "$ID_RSA"
+    sleep 10
+done
+
+echo "it's ok!"
+
+exec ssh $OPTS \
+    -i "$ID_RSA" -N -D :1080 "$@"
